e42d0fcf271b13990fc352b22f00cafeee8171ce
Mahesh Ravindra Kothawade <maheshkothavade123@gmail.com>
2024-10-20 00:08:08 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import LoginForm from './components/LoginForm';
+import ChatFeed  from './components/ChatFeed';
+
+const App = () =>{
+
+    if(!localStorage.getItem('username')) return <LoginForm/>
+    return (
+        <ChatEngine 
+            height='100vh'
+            projectID="6599cc4d-61f6-4fc1-9427-6987a168a44c"
+            // userName='maheshkothawade'
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+
+
+        />
+         
+    );
 }
 
 export default App;
